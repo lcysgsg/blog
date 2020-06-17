79d9ea453bb05e96bcf550156719b0b8fc8170a4
lcysgsg <249313573@qq.com>
2020-06-17 16:37:27 +0800

    update: eslintrc rules

--- eslint + prettier/.eslintrc.js
+++ eslint + prettier/.eslintrc.js
@@ -13,13 +13,16 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // 未使用声明，方便调试
+    "no-unused-vars": "warn",
 
     "prettier/prettier": [
       "error",
       {
         endOfLine: "auto",
+        // 比起页面空格产生的影响，我还是希望代码看起来简单一点
+        htmlWhitespaceSensitivity: "ignore",
       },
     ],
-    "vue/html-self-closing": "error",
   },
 };
